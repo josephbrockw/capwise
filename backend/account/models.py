cd3a9664cd76cf5cb0e324a13bd5641f5f287bbd
Joe Wilkinson <me@thejoewilkinson.com>
2024-10-10 09:38:26 -0500

    unique emails for users

--- backend/account/models.py
+++ backend/account/models.py
@@ -10,6 +10,7 @@ from django.utils.timezone import now
 
 class User(AbstractUser):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    email = models.EmailField(unique=True)
 
 
 class OneTimePassword(models.Model):
