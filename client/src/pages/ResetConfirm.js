25f4ad79d951f57fda0b0a62529b840f6a8ed468
Joe Wilkinson <me@thejoewilkinson.com>
2024-11-07 14:52:03 -0600

    remove commented out code

--- client/src/pages/ResetConfirm.js
+++ client/src/pages/ResetConfirm.js
@@ -75,52 +75,6 @@ const ResetConfirm = () => {
       </form>
     </AuthLayout>
   );
-  // return (
-  //   <div>
-  //     <div className="flex align-items-center justify-content-center">
-  //         {message  ? (
-  //           <div className="surface-card p-4 shadow-2 border-round w-full lg:w-6">
-  //             <div className="text-center mb-5">
-  //               <img src={inlineLogo} alt="hyper" height={50} className="mb-3 mt-3"/>
-  //               <div className="text-900 text-3xl font-medium mb-3">Password Reset Confirm</div>
-  //               <div className="text-600 font-medium line-height-3">{message}</div>
-  //               <Link to="/login" style={{textDecoration: 'none'}} data-cy="login-link">
-  //                 <span className="font-medium no-underline ml-2 text-blue-500 cursor-pointer">Sign in</span>
-  //               </Link>
-  //             </div>
-  //           </div>
-  //         ) : (
-  //           <div className="surface-card p-4 shadow-2 border-round w-full lg:w-6">
-  //             <div className="text-center mb-5">
-  //               <img src={inlineLogo} alt="hyper" height={50} className="mb-3 mt-3"/>
-  //               <div className="text-900 text-3xl font-medium mb-3">Confirm Password Reset</div>
-  //               <span className="text-600 font-medium line-height-3">Don't have an account?</span>
-  //               <Link to="/register" style={{textDecoration: 'none'}} data-cy="registration-link">
-  //                 <span className="font-medium no-underline ml-2 text-blue-500 cursor-pointer">Create today!</span>
-  //               </Link>
-  //             </div>
-  //             <div>
-  //               <div className="flex justify-content-center flex-wrap mb-3" data-cy="otp-container" >
-  //                 <InputOtp value={formData.token} onChange={handleOtpChange} length={6} mask/>
-  //               </div>
-  //
-  //               <FloatLabel id="password" label="Password" value={formData.password} onChange={handleChange} name="password" type="password" required />
-  //               <FloatLabel id="password_confirm" label="Password Confirm" value={formData.password_confirm} onChange={handleChange} name="password_confirm" type="password" required />
-  //
-  //               <div className="flex align-items-center justify-content-between mb-6">
-  //                 <Link to="/login" style={{textDecoration: 'none'}} data-cy="login-link">
-  //                   <span
-  //                     className="font-medium no-underline ml-2 text-blue-500 text-right cursor-pointer">Sign in</span>
-  //                 </Link>
-  //               </div>
-  //
-  //               <Button label="Submit" icon="pi pi-user" fullWidth onClick={handleSubmit} data-cy="submit-button" />
-  //             </div>
-  //           </div>
-  //         )}
-  //     </div>
-  //   </div>
-  // );
 };
 
 export default ResetConfirm;
