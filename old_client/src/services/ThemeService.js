73b55ef84bc35c0a3b5899bfb08dba64cea52b3f
Joe Wilkinson <me@thejoewilkinson.com>
2024-11-26 17:11:11 -0600

    remove old client

--- old_client/src/services/ThemeService.js
+++
@@ -1,7 +0,0 @@
-import { createTheme, ThemeProvider } from "@mui/material/styles";
-
-const defaultTheme = createTheme();
-
-export const SiteTheme = ({ children }) => {
-  return <ThemeProvider theme={defaultTheme}>{children}</ThemeProvider>;
-};
