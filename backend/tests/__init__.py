2e9a984725e77f410bdad904034cfde0e092c2a0
Joe Wilkinson <me@thejoewilkinson.com>
2025-03-12 11:39:14 -0500

    pretty print json data

--- backend/tests/__init__.py
+++ backend/tests/__init__.py
@@ -9,7 +9,7 @@ def read_api_response(response, show=False):
     err = response.get("error", "")
     err_code = response.get("error_code", None)
     if show:
-        print(f"Data:\n{data}")
+        print(f"Data:\n{json.dumps(data, indent=2)}")
         print(f"Message:  {msg}")
         print(f"Error:    {err}")
         print(f"Err Code: {err_code}")
